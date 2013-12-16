853af43721c7f8bbc75991725c306cedab3b1443
olli0601 (Dr ) <monsieur.olli@gmail.com>
2013-12-16 14:46:46 +0000

    added startme.R to run project files in shell. changed script files into functions.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colgem@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- prjcts/misc/clusterSize_SDE.R
+++ prjcts/misc/clusterSize_SDE.R
@@ -1,119 +1,20 @@
-#~ SDE version of model in clusterSizeVignette.R
-setwd('/Users/Oliver/workspace_sandbox/rcolgem')
-source('rcolgem.R')
-
-#~ parms_truth <<- list(gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 0.775, beta1=0.08, beta2=0.08, S0=2500, alpha = .05) 
-parms_truth <<- list(gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 1+1/30, beta1=(1+1/30)/10, beta2=(1+1/30)/10, S_0=5000, I0_0=1, I1_0=1, I2_0=1, alpha = 4) 
-FGYPARMS <<- parms_truth
-INFECTEDNAMES <<- c('I0', 'I1', 'I2')
-
-#~ the 'skeleton' functions are deterministic functions of system state
-F.skeleton <<- function(t, x = NA)
-{
-if (is.na(x[1]) )  x <- x.interp(t) 
-N <- sum(x) 
-I <- sum(x[INFECTEDNAMES])
-lambda <- exp(-FGYPARMS$alpha * I/N) * c( FGYPARMS$beta0*x['I0'], FGYPARMS$beta1*x['I1'] , FGYPARMS$beta2*x['I2'])  * x['S']/N
-unname( cbind( lambda, matrix(0, nrow=3, ncol=2) ) )
-}
-
-
-G. <<- function(t, x = NA)
-{
-if (is.na(x[1]) )  x <- x.interp(t) 
-g <- matrix(0, nrow=3, ncol=3)
-g[1,2] <- FGYPARMS$gamma0 * x['I0']
-g[2,3] <- FGYPARMS$gamma1 * x['I1']
-g
-}
-
-
-#~ solve using discrete euler method
-timestep = .1
-times <- seq(0, 50, by=timestep)
-
-step.x <- function(t, x, parms, ...) 
-{
-with(parms, {
-lambda <-  F.skeleton(t, x = x)
-lambdaTimestep <- lambda * timestep
-sumlambda <- sum(lambda) 
-sumlambdaTimestep <- sum(lambdaTimestep) 
-rsumlambda <- abs( sumlambda*timestep + sqrt(sumlambda) * rnorm(1, mean=0, sd=sqrt(timestep)) )
-lambda <- lambdaTimestep * rsumlambda/sumlambdaTimestep
-N <- sum(x)
-list( x = x + c(
-		S = -rsumlambda + timestep*b * N - timestep * mu * x['S'], 
-		I0 = rsumlambda - timestep * (mu+gamma0)*x['I0'],
-		I1 = timestep*gamma0*x['I0'] - timestep*(mu+gamma1)*x['I1'],
-		I2 = timestep*gamma1*x['I1'] - timestep*(mu+gamma2)*x['I2']) 
-	, lambda = lambda )
-})
-}
-
-
-solve.model.set.fgy <- function(parameters, shouldPlot=FALSE){ 
-	FGYPARMS <<- parameters
-	lambdas <- list()
-	x <-  c(S = parameters$S_0, I0=parameters$I0_0, I1=parameters$I1_0, I2=parameters$I2_0)
-	o <- c(0, x)
-	for (itimes in 2:length(times)){
-		t <- times[itimes]
-		x_lambda <- step.x( t, x, parameters)
-		x <- x_lambda[['x']]
-		lambdas[[itimes]] <- x_lambda[['lambda']]
-		o<- rbind(o, c(t, x))
-	}
-	x.interps<-  sapply( names(x), function(n) approxfun(o[,1], o[,n], yleft=o[1,n], yright=o[nrow(o),n]) )
-	x.interp <<- function(t) { sapply(x.interps, function(interp) interp(t) ) }  
-	Y. <<- function(t) x.interp(t)[INFECTEDNAMES]
-	F.interps <-  list()
-	for (k in 1:m){				#TODO not defined 
-		F.interps[[k]] <- list()
-		for (l in 1:m){
-			lambdaskl <- sapply(2:length(times), function(itime) lambdas[[itime]][k,l])
-			F.interps[[k]][[l]] <- approxfun( times[1:(length(times)-1)], lambdaskl/timestep, method='constant', rule=2)
-		}
-	}
-	F. <<- function(t.) t( sapply(1:m, function(k)  sapply(1:m, function(l) F.interps[[k]][[l]](t.)) ) )
-	if (shouldPlot){
-		class(o) <- 'deSolve'
-		#X11()
-		pdf( 'clusterSize_stochasticModel_vignette/epidemic.pdf')
-		plot(o)
-		dev.off()
-	}
-}
-
-# simulate coalescent tree with true parameters 
-solve.model.set.fgy(parms_truth, shouldPlot=TRUE) 
-phi = .50 # sample fraction
-sampleTime <- 50
-Y.sampleTime <- Y.(sampleTime)
-stateIndices <- rep( 1:3, round( phi * Y.sampleTime ) ) # sample each of three states in proportion to size
-n <- length(stateIndices)
-sampleTimes <- rep(sampleTime, n)
-sampleStates <- diag(3)[stateIndices,]
-coalescentTree_time <- system.time({
-  bdt <- simulatedBinaryDatedTree(sampleTime, sampleStates, discretizeRates=TRUE) 
-})
-
-# calculate empirical stats
-heights <- seq(0, 50, length.out=50)
-empiricalMoments = eM <- calculate.cluster.size.moments.from.tree(bdt, heights)
-
+################################################
+#
+#	script helper functions
+#
+################################################
 
 # comparison of model and empirical trees
-comparison.plots <- function(eM, mM)
+colgem.project.moments.for.sdesystem.comparison.plots <- function(eM, mM)
 {
 	X11()
 	par(mfrow=c(3,3))
 	for (i in 1:3){
 		for (j in 1:3){
 			plot( mM$heights, mM$M[i,j,], type='lines', log='y', main=paste(i,j), 
-			  ylim=c(1,
-					 max(c(mM$M[i,j,], eM[i,j,]) , na.rm = TRUE) )
-			  , col='red')
+					ylim=c(1,
+							max(c(mM$M[i,j,], eM[i,j,]) , na.rm = TRUE) )
+					, col='red')
 			lines( heights, eM[i,j,])
 		}
 	}
@@ -121,27 +22,28 @@ comparison.plots <- function(eM, mM)
 	plot( -mM$heights, rowSums( mM$A ), type='l' , col='red', main='Lineages through time')
 	ltt.lines(bdt) 
 }
-comparison.plots2 <- function(eM, nsims,parms=parms_truth)
+
+colgem.project.moments.for.sdesystem.comparison.plots2 <- function(eM, nsims,parms=parms_truth)
 {
 	mM <- list()
 	for (isim in 1:nsims){
 		mM_time <- system.time({  modelMoments = mM_i <- calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4')
-		})
+				})
 	}
 	mM <- lapply(1:nsims, function(isim) {
-	  solve.model.set.fgy(parms, shouldPlot=FALSE) ;
-	  calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4');
-	  }
-	 )
+				solve.model.set.fgy(parms, shouldPlot=FALSE) ;
+				calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4');
+			}
+	)
 	X11()
 	par(mfrow=c(4,3))
 	for (i in 1:3){
 		for (j in 1:3){
 			plot( heights, eM[i,j,], type='lines', log='y', main=paste(i,j), 
-			  ylim=c(1,
-					 max(c(mM$M[i,j,], eM[i,j,]) , na.rm = TRUE) )
-			  , col='black', 
-			  xlab='height')
+					ylim=c(1,
+							max(c(mM$M[i,j,], eM[i,j,]) , na.rm = TRUE) )
+					, col='black', 
+					xlab='height')
 			for (isim in 1:nsims){
 				lines(mM[[isim]]$heights, mM[[isim]]$M[i,j,], col='red')
 			}
@@ -153,30 +55,31 @@ comparison.plots2 <- function(eM, nsims,parms=parms_truth)
 		lines( -mM[[isim]]$heights, rowSums( mM[[isim]]$A ), col='red')
 	}
 }
-comparison.plots3<- function(eM, nsims,parms=parms_truth)
+
+colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, nsims, dir.name, parms=parms_truth)
 { # deltas
 	mM <- list()
 	for (isim in 1:nsims){
 		mM_time <- system.time({  modelMoments = mM_i <- calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4')
-		})
+				})
 	}
 	mM <- lapply(1:nsims, function(isim) {
-	  solve.model.set.fgy(parms, shouldPlot=FALSE) ;
-	  calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4');
-	  }
-	 )
+				solve.model.set.fgy(parms, shouldPlot=FALSE) ;
+				calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4');
+			}
+	)
 	#X11()
-	pdf( 'clusterSize_stochasticModel_vignette/delta_Mij.pdf')
+	pdf( paste(dir.name,'delta_Mij.pdf', sep='/') )
 	par(mfrow=c(3,3))
 	nheights <- length(heights)
 	for (i in 1:3){
 		for (j in 1:3){
 			emdeltas <- eM[i,j,2:nheights]-eM[i,j,1:(nheights-1)]
 			plot( heights[2:nheights], emdeltas, type='points', log='y', main=paste(i,j), 
-			  ylim=c(.01,
-					 max(emdeltas +10 , na.rm = TRUE) )
-			  , col='black', 
-			  xlab='height')
+					ylim=c(.01,
+							max(emdeltas +10 , na.rm = TRUE) )
+					, col='black', 
+					xlab='height')
 			for (isim in 1:nsims){
 				mmdeltas <- mM[[isim]]$M[i,j,2:nheights] - mM[[isim]]$M[i,j,1:(nheights-1)]
 				lines(mM[[isim]]$heights[2:nheights], mmdeltas, col='red')
@@ -187,7 +90,7 @@ comparison.plots3<- function(eM, nsims,parms=parms_truth)
 	
 	#residuals
 	#X11()
-	pdf( 'clusterSize_stochasticModel_vignette/residuals.pdf')
+	pdf( paste(dir.name,'residuals.pdf', sep='/') )
 	par(mfrow=c(3,3))
 	print('test')
 	for (i in 1:3){
@@ -196,8 +99,8 @@ comparison.plots3<- function(eM, nsims,parms=parms_truth)
 			isim <- 1
 			mmdeltas <- log(mM[[isim]]$M[i,j,2:nheights] - mM[[isim]]$M[i,j,1:(nheights-1)])
 			plot( heights[2:nheights], mmdeltas-emdeltas, type='points', main=paste(i,j), 
-			  col='black', 
-			  xlab='height')
+					col='black', 
+					xlab='height')
 			for (isim in 2:nsims){
 				mmdeltas <- log( mM[[isim]]$M[i,j,2:nheights] - mM[[isim]]$M[i,j,1:(nheights-1)] )
 				points(mM[[isim]]$heights[2:nheights], mmdeltas-emdeltas, col='black')
@@ -208,7 +111,7 @@ comparison.plots3<- function(eM, nsims,parms=parms_truth)
 	
 	#histogram of residuals
 	#X11()
-	pdf( 'clusterSize_stochasticModel_vignette/residuals_hist.pdf')
+	pdf( paste(dir.name,'residuals_hist.pdf', sep='/') )
 	par(mfrow=c(3,3))
 	for (i in 1:3){
 		for (j in 1:3){
@@ -227,15 +130,126 @@ comparison.plots3<- function(eM, nsims,parms=parms_truth)
 	dev.off()
 }
 
-comparison.plots3(eM , 20)
 
+colgem.project.moments.for.sdesystem<- function()
+{
+	my.mkdir(HOME, 'MOMSDE' )
+	dir.name		<- paste(HOME, 'MOMSDE',sep='/')
+	
+	#~ parms_truth <<- list(gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 0.775, beta1=0.08, beta2=0.08, S0=2500, alpha = .05) 
+	parms_truth <<- list(gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 1+1/30, beta1=(1+1/30)/10, beta2=(1+1/30)/10, S_0=5000, I0_0=1, I1_0=1, I2_0=1, alpha = 4) 
+	FGYPARMS <<- parms_truth
+	INFECTEDNAMES <<- c('I0', 'I1', 'I2')
+	
+	################################################
+	#
+	#	define F G solve.model.set.fgy that are assumed in rcolgem.R
+	#
+	################################################		
+	#~ the 'skeleton' functions are deterministic functions of system state
+	F.skeleton <<- function(t, x = NA)
+	{
+		if (is.na(x[1]) )  x <- x.interp(t) 
+		N <- sum(x) 
+		I <- sum(x[INFECTEDNAMES])
+		lambda <- exp(-FGYPARMS$alpha * I/N) * c( FGYPARMS$beta0*x['I0'], FGYPARMS$beta1*x['I1'] , FGYPARMS$beta2*x['I2'])  * x['S']/N
+		unname( cbind( lambda, matrix(0, nrow=3, ncol=2) ) )
+	}
+	G. <<- function(t, x = NA)
+	{
+		if (is.na(x[1]) )  x <- x.interp(t) 
+		g <- matrix(0, nrow=3, ncol=3)
+		g[1,2] <- FGYPARMS$gamma0 * x['I0']
+		g[2,3] <- FGYPARMS$gamma1 * x['I1']
+		g
+	}	
+	#~ solve using discrete euler method
+	timestep	<- .1
+	times 		<- seq(0, 50, by=timestep)	
+	step.x 		<- function(t, x, parms, ...) 
+	{
+		with(parms, {
+					lambda <-  F.skeleton(t, x = x)
+					lambdaTimestep <- lambda * timestep
+					sumlambda <- sum(lambda) 
+					sumlambdaTimestep <- sum(lambdaTimestep) 
+					rsumlambda <- abs( sumlambda*timestep + sqrt(sumlambda) * rnorm(1, mean=0, sd=sqrt(timestep)) )
+					lambda <- lambdaTimestep * rsumlambda/sumlambdaTimestep
+					N <- sum(x)
+					list( x = x + c(
+									S = -rsumlambda + timestep*b * N - timestep * mu * x['S'], 
+									I0 = rsumlambda - timestep * (mu+gamma0)*x['I0'],
+									I1 = timestep*gamma0*x['I0'] - timestep*(mu+gamma1)*x['I1'],
+									I2 = timestep*gamma1*x['I1'] - timestep*(mu+gamma2)*x['I2']) 
+							, lambda = lambda )
+				})
+	}
+	solve.model.set.fgy <- function(parameters, shouldPlot=FALSE)
+	{ 
+		FGYPARMS <<- parameters
+		lambdas <- list()
+		x <-  c(S = parameters$S_0, I0=parameters$I0_0, I1=parameters$I1_0, I2=parameters$I2_0)
+		o <- c(0, x)
+		for (itimes in 2:length(times)){
+			t <- times[itimes]
+			x_lambda <- step.x( t, x, parameters)
+			x <- x_lambda[['x']]
+			lambdas[[itimes]] <- x_lambda[['lambda']]
+			o<- rbind(o, c(t, x))
+		}
+		x.interps<-  sapply( names(x), function(n) approxfun(o[,1], o[,n], yleft=o[1,n], yright=o[nrow(o),n]) )
+		x.interp <<- function(t) { sapply(x.interps, function(interp) interp(t) ) }  
+		Y. <<- function(t) x.interp(t)[INFECTEDNAMES]
+		F.interps <-  list()
+		for (k in 1:m){				#TODO not defined 
+			F.interps[[k]] <- list()
+			for (l in 1:m){
+				lambdaskl <- sapply(2:length(times), function(itime) lambdas[[itime]][k,l])
+				F.interps[[k]][[l]] <- approxfun( times[1:(length(times)-1)], lambdaskl/timestep, method='constant', rule=2)
+			}
+		}
+		F. <<- function(t.) t( sapply(1:m, function(k)  sapply(1:m, function(l) F.interps[[k]][[l]](t.)) ) )
+		if (shouldPlot){
+			class(o) <- 'deSolve'
+			#X11()
+			pdf( 'clusterSize_stochasticModel_vignette/epidemic.pdf')
+			plot(o)
+			dev.off()
+		}
+	}
+	
+	################################################
+	#
+	#	start script
+	#
+	################################################
+	
+	
+	# simulate coalescent tree with true parameters 
+	solve.model.set.fgy(parms_truth, shouldPlot=TRUE) 
+	phi				<- .50 # sample fraction
+	sampleTime 		<- 50
+	Y.sampleTime 	<- Y.(sampleTime)
+	stateIndices 	<- rep( 1:3, round( phi * Y.sampleTime ) ) # sample each of three states in proportion to size
+	n 				<- length(stateIndices)
+	sampleTimes 	<- rep(sampleTime, n)
+	sampleStates 	<- diag(3)[stateIndices,]
+	coalescentTree_time <- system.time({
+				bdt <- simulatedBinaryDatedTree(sampleTime, sampleStates, discretizeRates=TRUE) 
+			})
+	# calculate empirical stats
+	heights 		<- seq(0, 50, length.out=50)
+	empiricalMoments= eM <- calculate.cluster.size.moments.from.tree(bdt, heights)
+	
+	colgem.project.moments.for.sdesystem.comparison.plots3(eM , 20, dir.name)	
+}
 
 
 # calculate model stats
 #~ mM_time <- system.time({
 #~   modelMoments = mM <- calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4')
 #~ })
-#~ comparison.plots(eM, mM) 
+#~ colgem.project.moments.for.sdesystem.comparison.plots(eM, mM) 
 
 #~ now do a comparison with model parameters different from true parameters 
 #~ parameters <- parms_truth
@@ -243,7 +257,7 @@ comparison.plots3(eM , 20)
 #~ parameters$beta1 <- parms_truth$beta1*5
 #~ solve.model.set.fgy(parameters)
 #~ modelMoments = mM <- calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4')
-#~ comparison.plots(eM, mM) 
+#~ colgem.project.moments.for.sdesystem.comparison.plots(eM, mM) 
 
 
 #~ compare computation times with likelihood
---
+++ prjcts/startme.R
@@ -0,0 +1,227 @@
+#! Rscript
+##	if the first line throws an error add the path to your Rscript to .profile,
+##	eg export PATH=/Library/Frameworks/R.framework/Versions/2.15/Resour\ces/bin:$PATH
+###############################################################################
+#
+#
+# Author: oliver ratmann
+# file: startme.R
+#
+# usage from R:
+#> setwd("/Users/Oliver/workspace_sandbox/colgem/prjcts")
+#> source("startme.R")
+# usage from bash:
+#> startme.R 
+#
+###############################################################################
+#	set these as needed
+CODE.HOME	<<- "/Users/Oliver/workspace_sandbox/colgem"
+HOME		<<- "/Users/Oliver/workspace_sandbox/colgem/prjcts"
+default.fun	<- "colgem.project.moments.for.odesystem"
+#default.fun	<- "colgem.project.moments.for.sdesystem"
+
+
+
+
+###############################################################################
+args <- commandArgs()
+if(!any(args=='--args'))
+	args<- vector("numeric",0)
+if(any(args=='--args'))
+	args<- args[-(1:match("--args", args)) ]
+###############################################################################
+DATA		<<- HOME
+DEBUG	<<- 0
+LIB.LOC		<<- NULL
+#LIB.LOC		<<- paste(CODE.HOME,"../",sep='')
+EPS			<<- 1e-12
+###############################################################################
+require(rcolgem)
+###############################################################################
+#	load all R functions and project source files. This avoids to rebuild the package all the time.
+function.list	<-c(	list.files(path= paste(CODE.HOME,"pkg/R",sep='/'), pattern = ".R$", all.files = FALSE, full.names = TRUE, recursive = FALSE),
+						list.files(path= paste(HOME,"misc",sep='/'), pattern = ".R$", all.files = FALSE, full.names = TRUE, recursive = FALSE)
+						)
+sapply(function.list,function(x) source(x,echo=FALSE,print.eval=FALSE, verbose=FALSE))
+###############################################################################
+my.mkdir<-function(root,data.name)
+{
+	if(length(dir(root,pattern=paste('^',data.name,'$',sep='')))==0)
+		system(paste("mkdir ",paste(root,data.name,sep='/'),sep=''))
+}
+###############################################################################
+my.dumpframes<- function()
+{
+	geterrmessage()
+	dump.frames()
+	cat(paste("\nmy.dumpframes dump 'last.dump' to file",paste(DATA,paste("debug_",paste(strsplit(date(),' ')[[1]],collapse='_'),".rda\n",sep=''),sep='/')))
+	save(last.dump, file=paste(DATA,paste("debug_",paste(strsplit(date(),' ')[[1]],collapse='_'),".rda",sep=''),sep='/'))
+	q()
+}
+###############################################################################
+my.make.documentation<- function()
+{
+	require(roxygen2)		
+	roxygenize(CODE.HOME)
+}
+###############################################################################
+my.fade.col<-function(col,alpha=0.5)
+{
+	return(rgb(col2rgb(col)[1]/255,col2rgb(col)[2]/255,col2rgb(col)[3]/255,alpha))
+}
+###############################################################################
+print.v<- function(x,cut=3,digits=4,prefix= "simu_",print.char= TRUE, as.R= TRUE)
+{
+	if(as.R)
+	{
+		tmp<- paste("c(",paste(c(x,recursive=T),collapse=',',sep=''),')',sep='')
+		if(!is.null(names(x)))
+			tmp<- paste("{tmp<-", tmp, "; names(tmp)<- ", paste('c("',paste(c(names(x),recursive=T),collapse='", "',sep=''),'")',sep=''), "; tmp}", sep= '', collapse= '')
+	}
+	else
+	{
+		if(!is.null(names(x)))
+		{
+			m<- matrix(NA,nrow=2,ncol=length(x))
+			m[1,]<- substr(names(x),1,cut)
+			m[2,]<- round( x, digits=digits )
+			if(cut==0)		m<- m[2,]
+			tmp<- gsub('.',',',paste(prefix,paste(as.vector(m), collapse='_',sep=''),sep=''),fixed=T)
+		}
+		else
+			tmp<- gsub('.',',',paste(prefix,paste(round( x, digits=digits ), collapse='_',sep=''),sep=''),fixed=T)
+	}
+	if(print.char) print(tmp)
+	tmp
+}
+###############################################################################
+plot.2D.dens<- function(x,y,xlab,ylab,xlim=NA,ylim=NA,nbin=NA,width.infl=2,n.hists=5,method="gauss", palette= "topo", persp.theta= -30, persp.phi= 30, zero.abline=TRUE, ...)
+{
+	if(!method%in%c("gauss","ash","persp"))	stop("plot.2D.dens: exception 1a")
+	if(!palette%in%c("topo","heat","gray"))	stop("plot.2D.dens: exception 1b")
+	switch(method,
+			gauss=
+					{
+						require(KernSmooth)
+						require(fields)
+						x.bw<- width.infl*diff(summary(x)[c(2,5)])
+						y.bw<- width.infl*diff(summary(y)[c(2,5)])
+						if(!x.bw) x.bw<- EPS
+						if(!y.bw) y.bw<- EPS
+						if(any(is.na(xlim)))	xlim<- range(x)+c(-1.5,1.5)*x.bw
+						if(any(is.na(ylim)))	ylim<- range(y)+c(-1.5,1.5)*y.bw
+						dens <- bkde2D(cbind(x, y), range.x=list(xlim,ylim),bandwidth=c(x.bw,y.bw))
+						contour(dens$x1, dens$x2, dens$fhat,xlab=xlab,ylab=ylab)
+						if(zero.abline) abline(v=0,col="black",lty=3,lwd=1.5)
+						if(zero.abline) abline(h=0,col="black",lty=3,lwd=1.5)
+					},
+			ash={
+				require(ash)
+				if(any(is.na(xlim))) xlim<- range(x)*1.05
+				if(any(is.na(ylim))) ylim<- range(y)*1.05
+				if(any(is.na(nbin))) nbin<- 2*c(nclass.Sturges(x),nclass.Sturges(y))
+				bins<- bin2(cbind(x, y), ab=rbind(xlim,ylim),nbin=nbin)
+				f <- ash2(bins,rep(n.hists,2))
+				#image(f$x,f$y,f$z, col=rainbow(50,start= 4/6,end=0),xlab=xlab,ylab=ylab)
+				if(palette=="topo")					image(f$x,f$y,f$z, col=tail( topo.colors(trunc(50*1.4)), 50 ),xlab=xlab,ylab=ylab,...)
+				else if(palette=="gray")			image(f$x,f$y,f$z, col=head( rev(gray(seq(0,.95,len=trunc(50*1.4)))), 50),xlab=xlab,ylab=ylab,...)					
+				else								image(f$x,f$y,f$z, col=heat.colors( 50 ),xlab=xlab,ylab=ylab,...)
+				contour(f$x,f$y,f$z,add=TRUE, nlevels= 5)
+				if(zero.abline) abline(v=0,col="black",lty=2,lwd=2)
+				if(zero.abline) abline(h=0,col="black",lty=2,lwd=2)
+			},
+			persp={
+				require(ash)
+				require(MASS)
+				if(any(is.na(xlim))) xlim<- range(x)*1.05
+				if(any(is.na(ylim))) ylim<- range(y)*1.05
+				bins<- bin2(cbind(x, y), ab=rbind(xlim,ylim),nbin=2*c(nclass.Sturges(x),nclass.Sturges(y)))
+				f <- ash2(bins,rep(n.hists,2))
+				
+				nrz <- nrow(f$z)
+				ncz <- ncol(f$z)
+				col<- tail(topo.colors(trunc(1.4 * 50)),50)
+				fcol      <- col[trunc(f$z / max(f$z)*(50-1))+1]
+				dim(fcol) <- c(nrz,ncz)
+				fcol      <- fcol[-nrz,-ncz]
+				par(mar=c(1/2,1/2,1/2,1/2))
+				persp(x=f$x,y=f$y,z=f$z,col= fcol,theta=persp.theta,phi=persp.phi,xlab=xlab,ylab=ylab,zlab='', ticktype= "detailed" )
+			})
+}
+###############################################################################
+plot.persplocfit<- function(x, pv, theta= 30, phi= 20, palette= "gray",tcl=-0.05,...)
+{	
+	d <- x$mi["d"]
+	ev <- x$mi["ev"]
+	where <- "grid"
+	
+	pv <- match(pv, x$vnames)
+	tv <- (1:d)[-pv]
+	vrs <- c(pv, tv)
+	if (any(duplicated(vrs))) 
+		warning("Duplicated variables in pv, tv")
+	if (any((vrs <= 0) | (vrs > d))) 
+		stop("Invalid variable numbers in pv, tv")
+	m <- ifelse(d == 1, 100, 40) 
+	m <- rep(m, d)
+	m[tv] <- mtv<- 6		
+	xl <- x$box
+	marg <- lfmarg(xl, m)
+	pred <- locfit:::preplot.locfit(x, marg, band = "none", tr = NULL, what = "coef", get.data = 0, f3d = 0)
+	z<- matrix(pred$fit, nrow = length(marg[[1]]))
+	nbcol <- 100
+	if(palette=="gray")	
+		color<- head( rev(gray(seq(0,0.95,len=trunc(nbcol*1.4)))), nbcol)
+	else
+	{
+		jet.colors <- colorRampPalette( c("blue", "green") )
+		color <- jet.colors(nbcol)
+	}
+	# Compute the z-value at the facet centres
+	nrz <- nrow(z)
+	ncz <- ncol(z)
+	zfacet <- z[-1, -1] + z[-1, -ncz] + z[-nrz, -1] + z[-nrz, -ncz]
+	# Recode facet z-values into color indices
+	facetcol <- cut(zfacet, nbcol)		
+	par(mar=c(0,2.5,0,0), tcl=tcl)
+	pmat<- persp(marg[[1]], marg[[2]], z, zlim= range(z)*1.1, col = color[facetcol], shade= 0.1, border=NA, ticktype = "detailed", ltheta = 120,theta = theta, phi = phi, expand = 0.75, box=1, ... )
+	
+	list(pmat=pmat, x= marg[[1]], y= marg[[2]], z= z)
+}
+###############################################################################
+#
+#
+#
+###############################################################################
+argv<- list()
+if(length(args))
+{
+	tmp<- na.omit(sapply(args,function(arg)
+					{
+						switch(substr(arg,2,4),
+								exe= return(substr(arg,5,nchar(arg))),
+								NA)
+					}))
+	if(length(tmp)!=0)
+	{
+		if(length(tmp)>1) stop("abc-n.startme.R: duplicate -exe")
+		else default.fun<- switch(tmp[1],
+					MAKE.DOCUMENTATION		 = "my.make.documentation",
+					MOM.ODE					 = "colgem.project.moments.for.odesystem",
+					MOM.SDE					 = "colgem.project.moments.for.sdesystem"
+					)
+	}
+	tmp<- na.omit(sapply(args,function(arg)
+					{
+						switch(substr(arg,2,6),
+								debug= 1,
+								NA)
+					}))		
+	if(length(tmp)!=0)	DEBUG<<- tmp[1]	
+	argv<<- args
+}
+###############################################################################
+if(DEBUG)	options(error= my.dumpframes)	
+cat(paste("\ncolgem.startme.R: ",ifelse(DEBUG,"debug",""),"call",default.fun))
+do.call(default.fun,list()) 	
+cat("\ncolgem: ",ifelse(DEBUG,"debug","")," end\n")
