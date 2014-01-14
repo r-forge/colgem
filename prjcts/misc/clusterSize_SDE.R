d160a46eedef3e9b5f037d9b794ef5115b3cd36a
olli0601 (Dr ) <monsieur.olli@gmail.com>
2014-01-14 12:54:24 +0000

    sde code works now from R console
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colgem@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- prjcts/misc/clusterSize_SDE.R
+++ prjcts/misc/clusterSize_SDE.R
@@ -58,12 +58,12 @@ colgem.project.moments.for.sdesystem.comparison.plots2 <- function(eM, nsims, so
 	}
 }
 
-colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, nsims, solve.model.set.fgy, dir.name, parms)
-{ # deltas
+colgem.project.moments.for.sdesystem.modelMoments<- function(parms, eM, sampleTime, sampleStates, solve.model.set.fgy, nsims)
+{
 	mM <- list()
 	for (isim in 1:nsims)
 	{
-		mM_time <- system.time({  
+		mM.time <- system.time({  
 					modelMoments = mM_i <- calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4')
 				})
 	}
@@ -72,12 +72,21 @@ colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, nsims, sol
 				solve.model.set.fgy(parms, shouldPlot=FALSE) ;
 				calculate.cluster.size.moments.from.model(sampleTime, sampleStates , timeResolution = 50, discretizeRates=TRUE, fgyResolution = 100 , integrationMethod = 'rk4');
 			})
-	#X11()
-	pdf( paste(dir.name,'delta_Mij.pdf', sep='/') )
-	par(mfrow=c(3,3))
-	nheights <- length(heights)
-	for (i in 1:3){
-		for (j in 1:3){
+	list(mM=mM, mM.time=mM.time)	
+}
+
+colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, mM, dir.name)
+{ 
+	# plot deltas
+	file	<- paste(dir.name,'delta_Mij.pdf', sep='/')
+	cat(paste("plot deltas to file=",file))
+	pdf( file )
+	par( mfrow=c(3,3) )
+	nheights 	<- length(heights)
+	for (i in 1:3)
+	{
+		for (j in 1:3)
+		{
 			emdeltas <- eM[i,j,2:nheights]-eM[i,j,1:(nheights-1)]
 			plot( heights[2:nheights], emdeltas, type='points', log='y', main=paste(i,j), 
 					ylim=c(.01,
@@ -92,9 +101,10 @@ colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, nsims, sol
 	}
 	dev.off()
 	
-	#residuals
-	#X11()
-	pdf( paste(dir.name,'residuals.pdf', sep='/') )
+	# plot residuals	
+	file	<- paste(dir.name,'residuals_Mij.pdf', sep='/')
+	cat(paste("plot residuals to file=",file))
+	pdf( file )
 	par(mfrow=c(3,3))
 	print('test')
 	for (i in 1:3){
@@ -114,8 +124,9 @@ colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, nsims, sol
 	dev.off()
 	
 	#histogram of residuals
-	#X11()
-	pdf( paste(dir.name,'residuals_hist.pdf', sep='/') )
+	file	<- paste(dir.name,'residualshist_Mij.pdf', sep='/')
+	cat(paste("plot residuals to file=",file))
+	pdf( file )
 	par(mfrow=c(3,3))
 	for (i in 1:3){
 		for (j in 1:3){
@@ -137,15 +148,20 @@ colgem.project.moments.for.sdesystem.comparison.plots3<- function(eM, nsims, sol
 
 colgem.project.moments.for.sdesystem<- function()
 {
+	
 	my.mkdir(HOME, 'MOMSDE' )
 	dir.name		<- paste(HOME, 'MOMSDE',sep='/')
 	
 	#~ parms_truth <<- list(gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 0.775, beta1=0.08, beta2=0.08, S0=2500, alpha = .05) 
-	parms_truth 	<<- list(gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 1+1/30, beta1=(1+1/30)/10, beta2=(1+1/30)/10, S_0=5000, I0_0=1, I1_0=1, I2_0=1, alpha = 4, m=3) 
+	parms_truth 	<<- list(	m=3, gamma0 = 1, gamma1 = 1/7, gamma2 = 1/2, mu = 1/30, b=.036, beta0 = 1+1/30, beta1=(1+1/30)/10, beta2=(1+1/30)/10, 
+								S_0=5000, I0_0=1, I1_0=1, I2_0=1, alpha = 4,
+								times=seq(0, 50, by=.1)) 
 	FGYPARMS 		<<- parms_truth
 	INFECTEDNAMES 	<<- c('I0', 'I1', 'I2')
-	timestep		<- .1
-	times 			<- seq(0, 50, by=timestep)
+	phi				<- .50 # sample fraction
+	sampleTime 		<- 50
+	nsims			<- 20
+	
 	################################################
 	#
 	#	define F G solve.model.set.fgy that are assumed in rcolgem.R
@@ -169,8 +185,9 @@ colgem.project.moments.for.sdesystem<- function()
 		g
 	}	
 	#~ solve using discrete euler method	
-	step.x 		<- function(t, x, timestep, parms ) 
+	step.x 		<- function(t, x, parms ) 
 	{
+		timestep	<- diff(parms$times)[1]
 		with(parms, {
 					lambda 				<- F.skeleton(t, x = x)
 					lambdaTimestep 		<- lambda * timestep
@@ -187,17 +204,19 @@ colgem.project.moments.for.sdesystem<- function()
 							lambda = lambda )
 				})
 	}
-	solve.model.set.fgy <- function(parameters, times, timestep, dir.name, shouldPlot=FALSE)
+	solve.model.set.fgy <- function(parameters, dir.name=NA, shouldPlot=FALSE)
 	{ 
-		FGYPARMS	<<- parameters
+		FGYPARMS	<<- parameters		
 		lambdas 	<- list()
-		x 			<-  c(S = parameters$S_0, I0=parameters$I0_0, I1=parameters$I1_0, I2=parameters$I2_0)		
+		x 			<-  c(S = parameters$S_0, I0=parameters$I0_0, I1=parameters$I1_0, I2=parameters$I2_0)
+		times		<- parameters$times
+		timestep	<- diff(parameters$times)[1]
 		m			<- 3
 		o 			<- c(0, x)
 		for (itimes in 2:length(times))
 		{
 			t 					<- times[itimes]
-			x_lambda 			<- step.x( t, x, timestep, parameters)
+			x_lambda 			<- step.x( t, x, parameters)
 			x 					<- x_lambda[['x']]
 			lambdas[[itimes]] 	<- x_lambda[['lambda']]
 			o					<- rbind(o, c(t, x))
@@ -216,7 +235,7 @@ colgem.project.moments.for.sdesystem<- function()
 			}
 		}
 		F. 			<<- function(t.) t( sapply(1:m, function(k)  sapply(1:parameters$m, function(l) F.interps[[k]][[l]](t.)) ) )
-		if (shouldPlot)
+		if(shouldPlot && !is.na(dir.name))
 		{
 			class(o) <- 'deSolve'
 			#X11()
@@ -234,23 +253,26 @@ colgem.project.moments.for.sdesystem<- function()
 	
 	
 	# simulate coalescent tree with true parameters 
-	solve.model.set.fgy(parms_truth, times, timestep, dir.name, shouldPlot=TRUE) 
-	phi				<- .50 # sample fraction
-	sampleTime 		<- 50
+	solve.model.set.fgy(parms_truth, dir.name, shouldPlot=TRUE) 
 	Y.sampleTime 	<- Y.(sampleTime)
 	m				<- 3
 	stateIndices 	<- rep( 1:m, round( phi * Y.sampleTime ) ) # sample each of three states in proportion to size
 	n 				<- length(stateIndices)
 	sampleTimes 	<- rep(sampleTime, n)
 	sampleStates 	<- diag(m)[stateIndices,]
+	cat(paste("\nsimulate structured coalescent tree, including tip states (0/1)"))
 	coalescentTree_time <- system.time({
 				bdt <- simulatedBinaryDatedTree(sampleTime, sampleStates, discretizeRates=TRUE) 
 			})
 	# calculate empirical stats
 	heights 		<- seq(0, 50, length.out=50)
+	cat(paste("\ncalculate moments of observed tree by averaging over lineages; tip states 0/1"))
 	empiricalMoments= eM <- calculate.cluster.size.moments.from.tree(bdt, heights)
-	
-	colgem.project.moments.for.sdesystem.comparison.plots3(eM , 20, solve.model.set.fgy, dir.name, parms_truth)	
+	cat(paste("\ncalculate moments under model"))
+	tmp				<- colgem.project.moments.for.sdesystem.modelMoments(parms_truth, eM, sampleTime, sampleStates, solve.model.set.fgy, nsims)
+	mM				<- tmp$mM
+	cat(paste("\nplot results"))
+	colgem.project.moments.for.sdesystem.comparison.plots3(eM , mM, dir.name)	
 }
 
 
